39df4fd09e8fa0615613b320e265869dfe28e119
André Domingues <dominguesstroppa@gmail.com>
2023-09-04 09:53:17 -0300

    [ADD] Route Guard in key param check

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,57 +27,28 @@ const router = createRouter({
     }
   ]
 })
-/*
-router.beforeEach((to, from, next) => {
-  console.log(to.name)
-  if (to.name === 'registerConfirm') {
-    const api = import.meta.env.VITE_API_URL;
-    const key = to.params.key //Get key from URL
-    console.log(key)
-    if (key) { //Verify if key exists
-      axios.get(api+'/users/register/'+key)
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    } 
-    
-    else { //If key not exists, redirect to home
-      next({ name: 'home' })
-    }
-  }
-  
-  next()
-})
-*/
 
 router.beforeEach((to, from, next) => {
   if (to.name === 'registerConfirm') {
-    const api = import.meta.env.VITE_API_URL;
-    const key = to.params.key //Get key from URL
-    console.log(api+'/users/register/'+key)
+   
+    const key = to.params.key //Get key from URL    
     if (key) { //Verify if key exists
-      axios.get(api+'/users/register/'+key, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
+      
+      axios.get('http://localhost:3333'+'/users/register/'+key)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err.response.status);
+        if(err.response.status == 400){
+          return { name: 'home' }
         }
+
       })
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        console.log(err);
-      })
+
     } 
-
-    else { //If key not exists, redirect to home
-      next({ name: 'home' })
-    }
-  }
-
+  }   
+  
   next()
 })
 
